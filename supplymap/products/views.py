d87ceb089789eb1acb6e60f15e563bf976a6068d
beechnut <cloydster@gmail.com>
2013-07-06 00:19:58 -0400

    Get ready for json serving

--- supplymap/products/views.py
+++ supplymap/products/views.py
@@ -1,2 +1,30 @@
 # Create your views here.
+
+# Add a view to link to the map here
 home_page = None
+
+# Add a views to serve up GeoJSON for a given product
+
+# class BlogJSON(JSONListView):
+
+#   def get_context_data(self, **kwargs):
+#         context = super(BlogJSON, self).get_context_data(**kwargs)
+#         context['countries'] = make_geojson([entry.author.pcvprofile.country for entry in self.object_list])
+#         return context
+        
+# def make_geojson(codes):
+#     features = []
+#     for code in set(codes):
+#         features.append({
+#             'type': "Feature",
+#             'geometry': {
+#                 'type': "Point",
+#                 'coordinates': data_options.COUNTRIES[code]['coords']
+#             },
+#             'properties': { 'title': code }
+#         })
+
+#     return {
+#         'type': "FeatureCollection",
+#         'features': features
+#     }
